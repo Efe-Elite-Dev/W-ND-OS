60f5c2d4f81ac0842ae197714453ad6725027e29
ELİTE <efe113878@gmail.com>
2026-05-17 02:13:17 +0300

    Update wind_subsystem.h

--- wind_subsystem.h
+++ wind_subsystem.h
@@ -3,17 +3,51 @@
 
 #include <stdint.h>
 
+/* GRUB'ın kernel.c'ye yolladığı Multiboot Donanım Yapısı */
+struct multiboot_info {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    uint32_t num;
+    uint32_t size;
+    uint32_t addr;
+    uint32_t shndx;
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    uint32_t drives_length;
+    uint32_t drives_addr;
+    uint32_t config_table;
+    uint32_t boot_loader_name;
+    uint32_t apm_table;
+    uint32_t vbe_control_info;
+    uint32_t vbe_mode_info;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
+    uint64_t framebuffer_addr;
+    uint32_t framebuffer_pitch;
+    uint32_t framebuffer_width;
+    uint32_t framebuffer_height;
+    uint8_t  framebuffer_bpp;
+    uint8_t  framebuffer_type;
+};
+
 /* Donanım Port Köprüleri */
 void outb(unsigned short port, unsigned char val);
 unsigned char inb(unsigned short port);
 
-/* Grafik ve Ekran Sürücü Fonksiyonları */
+/* Grafik ve Ekran Fonksiyonları */
 void init_graph_mode(void);
 void draw_pixel_pure(int x, int y, uint32_t color);
 void clear_screen_gfx(uint32_t color);
 void draw_window_pure(int x, int y, int width, int height, uint32_t border_color);
 
-/* Altsistem Fonksiyonları */
+/* Altsistemler */
 void clear_text_screen(void);
 void init_idt(void);
 void init_keyboard(void);
@@ -23,7 +57,7 @@ void check_keyboard_pure(void);
 void gui_refresh_desktop(void);
 void run_exe_subsystem(void);
 
-/* AI Tahmin Motoru Fonksiyonları */
+/* AI Modülleri */
 int ai_mouse_analyze_stress(void);
 int ai_keyboard_analyze_cadence(void);
 int ai_core_predict_scheduler(int stress, int cadence, int loops);
