50a069efabee2f7102cbdd8a13d5f0ac25a20fab
ELİTE <efe113878@gmail.com>
2026-05-17 23:15:29 +0300

    Update screen.h

--- screen.h
+++ screen.h
@@ -3,13 +3,14 @@
 
 #include <stdint.h>
 
-// Ekran çözünürlük ve VBE tampon tanımları
 #define SCREEN_WIDTH  320
 #define SCREEN_HEIGHT 200
 
-// Küresel VBE değişkenlerinin dış bildirimi (Extern)
 extern uint8_t* vbe_vram;
 extern uint32_t vbe_pitch;
+
+// Küresel piksel fonksiyonu prototipi
+void draw_pixel_pure(int x, int y, uint8_t color);
 
 void screen_init(void);
 void screen_put_pixel(int x, int y, uint8_t color);
