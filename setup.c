813448407a407ef4e96b2fa12ff516ad4e128e18
ELİTE <efe113878@gmail.com>
2026-05-18 21:52:13 +0300

    Update setup.c

--- setup.c
+++ setup.c
@@ -1,9 +1,7 @@
 #include "setup.h"
 
-// Donanımsal VGA Grafik Bellek Adresi (Makro çakışmasını önleyen temiz pointer)
 #define VGA_MEM_PTR ((uint8_t*)0xA0000)
 
-// Donanımsal Kesme Sürücü Fonksiyonu
 static inline void outb(uint16_t port, uint8_t val) {
     __asm__ volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
 }
@@ -13,7 +11,6 @@ static int button_press_depth = 0;
 static int wind_anim_frame = 0;
 
 void setup_init_palette(void) {
-    // Grafik kartı renk paletini Windland OS konseptine göre boyuyoruz
     outb(0x3C8, COLOR_WIND_BG);       outb(0x3C9, 58); outb(0x3C9, 59); outb(0x3C9, 61);
     outb(0x3C8, COLOR_WIND_CARD);     outb(0x3C9, 63); outb(0x3C9, 63); outb(0x3C9, 63);
     outb(0x3C8, COLOR_WIND_TEXT);     outb(0x3C9, 5);  outb(0x3C9, 10); outb(0x3C9, 20);
@@ -37,86 +34,60 @@ void setup_draw_rect(int x, int y, int w, int h, uint8_t color) {
     }
 }
 
-// Harika Kıvrımlı Rüzgarlı Fırtına Logoları Çizen Fonksiyon (Wind Swirls)
 void draw_wind_swirl_vector(int cx, int cy, int size) {
     for (int i = 0; i < size; i++) {
         int offset_x = (i * i) / 20 + wind_anim_frame;
         int offset_y = i - (size / 2);
         setup_put_pixel(cx + offset_x, cy + offset_y, COLOR_WIND_SWIRL);
         setup_put_pixel(cx - offset_x, cy - offset_y, COLOR_WIND_TEXT);
-        setup_put_pixel(cx + offset_y, cy - offset_x / 2, COLOR_WIND_SWIRL);
     }
 }
 
-// Fluent / 3D Hibrit Panel Çizici
 void draw_window_card(int x, int y, int w, int h) {
     setup_draw_rect(x, y, w, h, COLOR_WIND_CARD);
     for (int i = 0; i < w; i++) {
         setup_put_pixel(x + i, y + h, COLOR_WIND_3D_DARK);
-        setup_put_pixel(x + i, y + h + 1, COLOR_WIND_3D_DARK);
     }
     for (int i = 0; i < h; i++) {
         setup_put_pixel(x + w, y + i, COLOR_WIND_3D_DARK);
-        setup_put_pixel(x + w + 1, y + i, COLOR_WIND_3D_DARK);
     }
 }
 
-// Mekanik Çökme Animasyonuna Sahip Akıllı Buton Sürücüsü
-void draw_responsive_button(int x, int y, int w, int h, const char* label) {
-    (void)label;
+void draw_responsive_button(int x, int y, int w, int h) {
     int depth = button_press_depth;
     setup_draw_rect(x, y, w, h, COLOR_WIND_3D_DARK); 
     setup_draw_rect(x + depth, y + depth, w - depth, h - depth, COLOR_WIND_PRIMARY);
 }
 
 void setup_render(void) {
-    // 1. Windland OS Gradyan Esintili Arka Plan
     setup_draw_rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_WIND_BG);
-    
-    // 2. Ana İşlem Kartı (Center Panel)
     draw_window_card(20, 15, 280, 170);
 
-    // 3. Kademelere Göre Görsel Vektör ve Form Çizimleri
     switch (current_stage) {
         case STAGE_WIND_COUNTRY:
             draw_wind_swirl_vector(80, 90, 40);
-            setup_draw_rect(150, 40, 120, 15, COLOR_WIND_BG);
             setup_draw_rect(150, 60, 120, 15, COLOR_WIND_PRIMARY); 
-            setup_draw_rect(150, 80, 120, 15, COLOR_WIND_BG);
             break;
-            
         case STAGE_WIND_KEYBOARD:
             setup_draw_rect(50, 70, 60, 40, COLOR_WIND_BG);
-            for(int k=0; k<4; k++) setup_draw_rect(55, 75 + (k*8), 50, 4, COLOR_WIND_TEXT);
             setup_draw_rect(150, 50, 120, 15, COLOR_WIND_PRIMARY); 
             break;
-            
         case STAGE_WIND_NETWORK:
             draw_wind_swirl_vector(70, 90, 30);
             setup_draw_rect(140, 75, 130, 12, COLOR_WIND_BG); 
             break;
-            
         case STAGE_WIND_HOSTNAME:
             setup_draw_rect(40, 80, 50, 35, COLOR_WIND_TEXT);
-            setup_draw_rect(35, 115, 60, 5, COLOR_WIND_PRIMARY);
-            setup_draw_rect(130, 85, 140, 15, COLOR_WIND_CARD); 
-            setup_draw_rect(130, 100, 80, 2, COLOR_WIND_PRIMARY); 
+            setup_draw_rect(130, 85, 140, 15, COLOR_WIND_BG); 
             break;
-            
         case STAGE_WIND_CUSTOMIZE:
-            for(int i=0; i<3; i++) {
-                setup_draw_rect(140, 45 + (i * 25), 10, 10, COLOR_WIND_BG);
-                if (i == 0) setup_draw_rect(142, 47, 6, 6, COLOR_WIND_PRIMARY); 
-            }
+            setup_draw_rect(140, 45, 10, 10, COLOR_WIND_PRIMARY);
             break;
-            
         default:
             draw_wind_swirl_vector(160, 100, 60);
             break;
     }
-
-    // Alt Navigasyon "Sonraki / İleri" Mekanik Butonu
-    draw_responsive_button(220, 155, 65, 20, "Ileri");
+    draw_responsive_button(220, 155, 65, 20);
 }
 
 void setup_delay(int count) {
@@ -131,19 +102,27 @@ void trigger_next_stage(void) {
         setup_render();
         setup_delay(8);
     }
-    
     wind_anim_frame += 4;
-    if (current_stage < STAGE_WIND_COMPLETE) {
-        current_stage++;
-    } else {
-        current_stage = STAGE_WIND_COUNTRY; 
-    }
+    if (current_stage < STAGE_WIND_COMPLETE) current_stage++;
+    else current_stage = STAGE_WIND_COUNTRY; 
     
     button_press_depth = 0;
     setup_render();
 }
 
 void setup_init(void) {
+    // Portlar üzerinden donanımsal Mode 13h Grafik modunu açıyoruz
+    outb(0x3C2, 0x63);
+    uint8_t vga_regs[] = {
+        0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+        0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x9C, 0x0E, 0x8F, 0x28, 0x40, 0x96, 0xB9, 0xA3, 0xFF
+    };
+    for (uint8_t i = 0; i < 24; i++) {
+        outb(0x3D4, i);
+        outb(0x3D5, vga_regs[i]);
+    }
+
     setup_init_palette();
     setup_render();
 }
