ad29adf724d2601d0dd9a375cafb260cbce7d77b
ELİTE <efe113878@gmail.com>
2026-05-20 00:08:46 +0300

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -1,16 +1,32 @@
 section .multiboot
 align 4
     MULTIBOOT_MAGIC    equ 0x1BADB002
-    ; Flagler: bit 0 (hizala) + bit 1 (bellek haritası) + bit 2 (grafik modu isteği)
-    MULTIBOOT_FLAGS    equ 0x00000007 
+    MULTIBOOT_FLAGS    equ 0x00000007 ; Bit 0 (hizala) + Bit 1 (mem info) + Bit 2 (video)
     MULTIBOOT_CHECKSUM equ -(MULTIBOOT_MAGIC + MULTIBOOT_FLAGS)
 
+    ; 1. Magic (Offset 0)
     dd MULTIBOOT_MAGIC
+    ; 2. Flags (Offset 4)
     dd MULTIBOOT_FLAGS
+    ; 3. Checksum (Offset 8)
     dd MULTIBOOT_CHECKSUM
 
-    ; EĞER BIT 2 AKTİFSE BU ALANLAR TAM OLARAK BU SIRAYLA OLMALIDIR:
-    dd 0          ; mode_type: 0 = Lineer Grafik Modu (VBE)
-    dd 1024       ; width (Genişlik)
-    dd 768        ; height (Yükseklik)
-    dd 32         ; depth (Renk derinliği - bpp)
+    ; --- ZORUNLU PADDING (AOUT KLUDGE ALANLARI) ---
+    ; Bit 16 kapalı olsa bile GRUB'ın 32. byte'a kadar kaymaması için bu 5 alan SIFIR olmalıdır!
+    dd 0    ; header_addr   (Offset 12)
+    dd 0    ; load_addr     (Offset 16)
+    dd 0    ; load_end_addr (Offset 20)
+    dd 0    ; bss_end_addr  (Offset 24)
+    dd 0    ; entry_addr    (Offset 28)
+
+    ; --- GRAFİK MODU ALANI (Tam olarak Offset 32'de olmak zorunda) ---
+    dd 0    ; mode_type (0 = Lineer VBE Modu)
+    dd 1024 ; width
+    dd 768  ; height
+    dd 32   ; depth (bpp)
+
+; Buradan sonra normal kernel girişin başlayabilir
+global _start
+_start:
+    cli
+    ; ...
