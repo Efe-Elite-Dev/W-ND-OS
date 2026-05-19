b5686ecb6e91ec8dc1bc9725a0af05eaefd76396
ELİTE <efe113878@gmail.com>
2026-05-19 16:32:24 +0300

    Create globals.h

---
+++ globals.h
@@ -0,0 +1,10 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdint.h>
+
+/* Global Değişken Tanımları */
+extern uint32_t* GRAPHICS_FRAMEBUFFER;
+extern int is_graphics_mode;
+
+#endif // GLOBALS_H
