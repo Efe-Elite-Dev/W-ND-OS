e5aaddc0a137acf261a83690e2051ce4c8460f03
ELİTE <efe113878@gmail.com>
2026-05-19 18:04:01 +0300

    Create types.h

---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef int bool;
+#define true 1
+#define false 0
+#define NULL ((void*)0)
+
+#endif
