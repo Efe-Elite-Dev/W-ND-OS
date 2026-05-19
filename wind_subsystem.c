4b1521083c32c7901607711b57cd5c5ecafb9a62
ELİTE <efe113878@gmail.com>
2026-05-19 19:48:58 +0300

    Update wind_subsystem.c

--- wind_subsystem.c
+++ wind_subsystem.c
@@ -1,17 +1,7 @@
-// wind_subsystem.c - Wind OS / Sky Core OS Grafik Alt Sistemi (Saf C)
-#include <stdbool.h>
+#ifndef WIND_SUBSYSTEM_H
+#define WIND_SUBSYSTEM_H
+#include "globals.h"
 
-// Dışarıdan çağrılacak ekran fonksiyonları prototipleri
-extern void draw_rectangle(int x, int y, int width, int height, unsigned int color);
-extern void draw_string(const char* text, int x, int y, unsigned int color);
-
-// Wind Grafik Alt Sistemini Başlatma Fonksiyonu
-void init_wind_subsystem(void) {
-    // İleride buraya pencere yöneticisi (window manager) kodlarını ekleyeceksin
-    draw_string("Wind Subsystem [ONLINE]", 10, 10, 0x00FF00);
-}
-
-// Pencereleri güncelleme döngüsü
-void update_wind_windows(void) {
-    // Pencere taşıma, kapatma ve render işlemleri buraya gelecek
-}
+// Buraya sadece "wind" ile ilgili özgün fonksiyonları bırak
+void init_wind_subsystem();
+#endif
