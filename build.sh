25c5a7e32b86212fa87e21563418991b8d9d523b
ELİTE <efe113878@gmail.com>
2026-05-20 22:27:09 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -e
-
-echo "[+] Boot grafik kesiti derleniyor..."
+echo "[1] boot.asm derleniyor..."
 nasm -f elf32 boot.asm -o boot.o
 
-echo "[+] Nesne dosyalari baglaniyor (Linking)..."
-ld -m elf_i386 -T linker.ld -o kernel.bin boot.o kernel.o
+echo "[2] kernel.c derleniyor..."
+gcc -m32 -ffreestanding -fno-builtin -fno-stack-protector -O2 -Wall -c kernel.c -o kernel.o
 
-echo "[+] ISO dizin yapisi olusturuluyor..."
-mkdir -p isodir/boot/grub
-cp kernel.bin isodir/boot/kernel.bin
-cp grub.cfg isodir/boot/grub/grub.cfg
+echo "[3] Linkleniyor..."
+ld -m elf_i386 -T linker.ld boot.o kernel.o -o kernel.bin
 
-echo "[+] Boot edilebilir OOBE/Desktop ISO uretiliyor..."
-grub-mkrescue -o windos.iso isodir
+echo "[4] ISO yapısı oluşturuluyor..."
+mkdir -p iso/boot/grub
+cp kernel.bin iso/boot/kernel.bin
+cp grub.cfg   iso/boot/grub/grub.cfg
 
-echo "[+] Temizlik yapiliyor..."
-rm -rf boot.o kernel.o kernel.bin isodir
+echo "[5] ISO üretiliyor..."
+grub-mkrescue -o windos.iso iso
 
-echo "[===] BASARILI: windos.iso grafik modu icin hazir! [===]"
+echo "[6] Temizleniyor..."
+rm -rf boot.o kernel.o kernel.bin iso
+
+echo ""
+echo "=== BASARILI: windos.iso hazir ==="
